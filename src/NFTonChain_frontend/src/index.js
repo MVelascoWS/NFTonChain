7364c95b73d2629b49b72ae6d161cc88d4ef1eb9
Michael Velasco <mvelasco@wasdstudio.com>
2023-09-18 11:57:16 -0600

    Added a little interaction demo in the frontend

--- src/NFTonChain_frontend/src/index.js
+++ src/NFTonChain_frontend/src/index.js
@@ -1,19 +1,55 @@
 import { NFTonChain_backend } from "../../declarations/NFTonChain_backend";
+import { IDL } from "@dfinity/candid";
+import { Principal } from "@dfinity/principal";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
+document.querySelector("form.balance").addEventListener("submit", async (e) => {
   e.preventDefault();
   const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
+  
+  const identity = document.getElementById("identity").value.toString();
+  const principal = Principal.fromText(identity);
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await NFTonChain_backend.greet(name);
+  // Interact with foo actor, calling the balance method
+  const balance = await NFTonChain_backend.balanceOfDip721(principal);
+  button.removeAttribute("disabled");
+
+  document.getElementById("balance").innerText = balance;
+
+  return false;
+});
+
+document.querySelector("form.limit").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+  
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the maxLimit method
+
+  const maxLimit = await NFTonChain_backend.getMaxLimitDip721();
 
   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+  document.getElementById("limit").innerText = maxLimit;
+
+  return false;
+});
+
+document.querySelector("form.token").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+  
+  const tokenID = document.getElementById("tokenID").value.toString();
+  let x = BigInt(tokenID);
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the owner method
+  const owner = await NFTonChain_backend.ownerOfDip721(x);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("owner").innerText = owner;
 
   return false;
 });
